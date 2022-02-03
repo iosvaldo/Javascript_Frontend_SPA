7dc677c120b981a883378838ebd6ad52c6344d7b
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-02-03 11:31:11 -0500

    Heroku backend server

--- frontend/static/views/Config.js
+++ frontend/static/views/Config.js
@@ -1,3 +1,5 @@
-export const APIPath = "http://localhost:3000/toys/";
+// export const APIPath = "http://localhost:3000/toys/";
 
-// export const APIPath = "https://json-server-template-1.herokuapp.com/toys/";
+export const APIPath = "https://json-server-template-1.herokuapp.com/toys/";
+
+
